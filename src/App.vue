519dc71ce226a970bcfb7bf7d06927930f34fde2
turek695 <turowski.luk@gmail.com>
2025-06-22 19:13:55 +0200

    Add converter input component
    Added ConverterInput.vue component with basic markup, bindings and styles.
    Added  inputs to App.vue to ensure proper behavior.

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,21 @@
   </header>
 
   <main>
+    <ConverterInput v-model:value="fromValue" v-model:currency="fromCurrency" :currencies="currencies"/>
+    <ConverterInput v-model:value="toValue" v-model:currency="toCurrency" :currencies="currencies"/>
   </main>
 </template>
 
 <script setup>
+import {ref} from 'vue';
+import ConverterInput from '@/components/inputs/ConverterInput.vue';
 
+const fromValue = ref(0);
+const fromCurrency = ref(0);
+const toValue = ref(0);
+const toCurrency = ref(0);
+
+const currencies = ref(['pln', 'eur']);
 </script>
 
 <style scoped>
---
+++ src/components/inputs/ConverterInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="converter-input">
+        <input v-model="value" type="number" min="0">
+        <select v-model="currency">
+            <option v-for="currency in props.currencies" :value="currency" :key="currency">{{ currency }}</option>
+        </select>
+    </div>
+</template>
+
+<script setup>
+import { defineModel, defineProps } from 'vue';
+
+const props = defineProps({
+    currencies: {
+        type: Array,
+        default() { () => [] },
+    }
+})
+const value = defineModel('value');
+const currency = defineModel('currency');
+</script>
+
+<style scoped>
+.converter-input {
+    background-color: grey;
+    padding: 4px;
+    display: flex;
+    gap: 8px;
+    border-radius: 4px;
+    width: max-content;
+    margin: 8px 0;
+}
+</style>
